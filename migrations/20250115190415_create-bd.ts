ee9d16282ff607b60f2e11d52f80002daee192ed
leonardo <leodsmf@gmail.com>
2025-01-15 16:54:44 -0300

    criando o banco de dados

--- migrations/20250115190415_create-bd.ts
+++ migrations/20250115190415_create-bd.ts
@@ -1,5 +1,15 @@
 import { Knex } from 'knex'
 
-export async function up(knex: Knex): Promise<void> {}
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meal', (table) => {
+    table.uuid('id').primary()
+    table.text('name').notNullable()
+    table.text('description')
+    table.boolean('diet').notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+  })
+}
 
-export async function down(knex: Knex): Promise<void> {}
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meal')
+}
--- src/database.ts
+++ src/database.ts
@@ -1,19 +1,21 @@
+console.log('test')
 import { Knex, knex as setup } from 'knex'
 import 'dotenv/config'
 if (!process.env.DATABASE_URL || !process.env.DATABASE_URL.endsWith('.db')) {
   throw new Error('Not found')
 }
+console.log('test')
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
     filename: process.env.DATABASE_URL,
   },
+  useNullAsDefault: true,
   migrations: {
     extension: 'ts',
-    directory: './db/migrations',
+    directory: 'migrations',
   },
-  useNullAsDefault: true,
 
 }
 
