3c37626cc34b3a68307fd28d9e96e555a02e262f
leonardo.mendes <leodsmf@gmail.com>
2025-01-20 15:36:55 -0300

    migrate create-user

---
+++ migrations/20250116223551_create-user.ts
@@ -0,0 +1,15 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('user', (table) => {
+    table.uuid('id').primary().notNullable()
+    table.text('name').notNullable()
+    table.text('email')
+    table.text('senha')
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('user')
+}
